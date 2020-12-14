e9005ac93401b28026ec8817d448018d0eb1d042
vzabavski <vzabavski123@gmail.com>
2020-12-14 20:57:52 +0300

    refactor: delete table.js

--- src/scripts/table.js
+++
@@ -1,7 +0,0 @@
-import {CountryData} from './countries.data.js'
-CountryData.fetching()
-setTimeout(() => console.log(CountryData.countries[5]['name']), 4000)
-
-import {StatsData} from './stats.data.js'
-StatsData.getInfo('Belarus', 'total by country')
-setTimeout(() => console.log(StatsData.countryTotalConfirmed), 3000)
