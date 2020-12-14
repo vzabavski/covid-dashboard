fce60e2dc6b70b3a028c44a09ccbfa881017a729
vzabavski <vzabavski123@gmail.com>
2020-12-14 20:48:49 +0300

    feat: add basic files structure, add fetching modules

---
+++ src/scripts/countries.data.js
@@ -0,0 +1,17 @@
+export const CountryData = {
+    countries: '',
+    fetching: function() {
+        fetch('https://restcountries.eu/rest/v2/all?fields=name;population;flag')
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          this.countries = data;
+          /* Добавить функцию по списка */
+        });
+    },
+}
+
+
+ 
+
